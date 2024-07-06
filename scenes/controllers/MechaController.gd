65cca97deb72adcf881468f9316d8bbf18066813
Oscar Pindaro <oscar.pindaro@gmail.com>
2024-07-06 10:05:43 +0200

    created a map node, removed a test boolean on the player that was causing problems while movin and created test movement scene

--- scenes/controllers/MechaController.gd
+++ scenes/controllers/MechaController.gd
@@ -8,11 +8,15 @@ extends Node2D
 
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed():
-		var mouse_position: Vector2 = get_local_mouse_position()
-		print("aaaa", map_node.tilemap	)
+		var mouse_position: Vector2 = get_global_mouse_position()
 		var target_cell: Vector2i = get_coord_in_map(mouse_position)
 
 		var mecha_cell: Vector2i = get_coord_in_map(curr_mecha.position)
+		
+		# print("Target Cell", target_cell)
+		# print("Mecha Cell", mecha_cell)
+		# print("Differen Cells", target_cell != mecha_cell)
+
 
 		if target_cell != mecha_cell:
 			curr_mecha.move_to(get_center_tile_pos_from_cord(target_cell))
@@ -27,7 +31,9 @@ func get_coord_in_map(pos: Vector2) -> Vector2i:
 	return map_node.tilemap.local_to_map(localized)
 
 func get_center_tile_pos_from_cord(coord: Vector2i) -> Vector2:
-	return map_node.tilemap.map_to_local(coord)
+	var out_pos_local: Vector2  =map_node.tilemap.map_to_local(coord)
+	var out_pos_global: Vector2 = get_parent().to_global(out_pos_local)
+	return out_pos_global
 	
 
 # Called when the node enters the scene tree for the first time.
