89fe87827db32d5f1539671dc22c7b8bbb3524e9
Matteo Mangioni <mat.mangioni@gmail.com>
2024-07-06 20:43:22 +0200

    Add basic difficulty increase

--- scenes/game/game_manager.gd
+++ scenes/game/game_manager.gd
@@ -5,13 +5,19 @@ extends Node
 @export var goal_point : Marker2D
 
 # Wave data
+@export var start_spawn_value : float = 100
+@export var start_spawn_timer : float = 1
+@export var spawn_value_percentage_increase : float = 0.05
+@export var spawn_timer_percentage_reduce : float = 0.05
+var spawn_value : float
+var spawn_timer : float
 var wave_num : int = 0
+var wave_in_progress : bool = false
 
-# Health data
+# Game data
 @export var health : float = 100
-
-# Money data
 @export var money : int = 3000
+var is_game_over : bool = false
 
 # Status signals
 signal game_start()
@@ -26,6 +32,9 @@ func _ready():
 	# Set spawner variables
 	$Spawner.spawn_point = spawn_point
 	$Spawner.goal_point = goal_point
+
+	spawn_value = start_spawn_value
+	spawn_timer = start_spawn_timer
 
 	# Connect to spawner signals
 	$Spawner.start_spawning.connect(_on_start_spawning)
@@ -56,11 +65,26 @@ func lose_health(value):
 	health_change.emit(value, health)
 	if health <= 0:
 		game_over.emit()
+		is_game_over = true
 
 func start_wave():
-	wave_num += 1
-	$Spawner.spawn_wave(wave_num, 10, 1) # TODO: build parameter functions
-	wave_start.emit(wave_num)
+	if !wave_in_progress && !is_game_over:
+		wave_in_progress = true
+		wave_num += 1
+
+		if wave_num != 1:	# Update difficulty
+			spawn_value = spawn_value * (1 + spawn_value_percentage_increase)
+			spawn_timer = spawn_timer * (1 - spawn_timer_percentage_reduce)
+
+		$Spawner.spawn_wave(wave_num, spawn_value, spawn_timer)
+		wave_start.emit(wave_num)
+		print("Started wave ", wave_num, " with params: ", spawn_value, " ", spawn_timer)
+
+func _check_wave_end():
+	if $Spawner/Enemies.get_child_count() == 0:
+		wave_in_progress = false
+		wave_end.emit(wave_num)
+		$WaveEndTimer.stop()
 
 func _on_start_spawning():
 	pass
@@ -71,8 +95,3 @@ func _on_spawned_enemy(enemy):
 
 func _on_stop_spawning():
 	$WaveEndTimer.start()
-
-func _check_wave_end():
-	if $Spawner/Enemies.get_child_count() == 0:
-		wave_end.emit(wave_num)
-		$WaveEndTimer.stop()
