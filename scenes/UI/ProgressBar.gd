4cddd8d5a27b1881a15391b497ba457624bf956e
Franco Forte <samuele.loiacono@gmail.com>
2024-07-07 11:40:17 +0200

    commit per checkout

--- scenes/UI/ProgressBar.gd
+++ scenes/UI/ProgressBar.gd
@@ -1,5 +1,11 @@
 extends ProgressBar
 
+
 func change_health(damage: int):
 	var tween = create_tween()
 	tween.tween_property(self, 'value', value - damage, 0.6)
+
+
+func _on_game_manager_health_change(delta, tot):
+	var tween = create_tween()
+	tween.tween_property(self, 'value', tot, 0.6)
