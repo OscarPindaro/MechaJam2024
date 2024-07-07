70e3a86b848c2e1f0735a1c0eb51f73ebd28b0c5
Fabio Losavio <fabio.losavio@hotmail.it>
2024-07-07 12:35:59 +0200

    fixed in game menu settings screen, now has a transparent background and works with the button

--- scenes/UI/Menu.gd
+++ scenes/UI/Menu.gd
@@ -1,18 +1,27 @@
 extends Button
 
 #var next_level_resource = load("res://scenes/UI/InGameSettings.tscn")
-
-
+var is_menu_screen_loaded = false
+var menu_scene = preload("res://scenes/UI/InGameSettings.tscn").instantiate()
 
 
 func _on_pressed():
 	var tree = get_tree()
-	tree.paused = tree.paused
-	#var scene = load("res://scenes/UI/InGameSettings.tscn")
 
+	if is_menu_screen_loaded == false:
+		tree.paused = true
+		
 
-	#scene.instantiate()
+		tree.get_root().add_child(menu_scene)
+		is_menu_screen_loaded = true
+		
+	else:
+		tree.paused = false
+		tree.get_root().remove_child(menu_scene)
+		is_menu_screen_loaded = false
+		
+	
+	# FUNZIONA MA SOSTITUISCE LA SCENA COMPETAMENTE
+	#tree.change_scene_to_file("res://scenes/UI/InGameSettings.tscn")
 
-#	var next_level = next_level_resource.instantiate()
-#	add_child(next_level)
 #
