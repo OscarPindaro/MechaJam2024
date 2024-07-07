bd08fe8a027f2e84c1db6023f6ada6f7c89b7d52
Matteo Mangioni <mat.mangioni@gmail.com>
2024-07-07 19:01:05 +0200

    Connect health and money

--- scenes/UI/ProgressBar.gd
+++ scenes/UI/ProgressBar.gd
@@ -1,11 +1,9 @@
 extends ProgressBar
 
-
 func change_health(damage: int):
 	var tween = create_tween()
 	tween.tween_property(self, 'value', value - damage, 0.6)
 
-
-func _on_game_manager_health_change(delta, tot):
+func _on_game_manager_health_change(_delta, tot):
 	var tween = create_tween()
 	tween.tween_property(self, 'value', tot, 0.6)
--- scenes/UI/UserInterface.gd
+++ scenes/UI/UserInterface.gd
@@ -7,7 +7,13 @@ func _ready():
 	if is_instance_valid(game_manager):
 		# Setup wave button
 		%StartWaveButton.button_up.connect(make_wave_start)
-		game_manager.wave_end.connect(func(_wave) : %StartWaveButton.disabled = false )
+		game_manager.wave_end.connect(func(_wave) : %StartWaveButton.disabled = false)
+
+		# Connect money and health
+		game_manager.health_change.connect(%HPProgressbar._on_game_manager_health_change)
+
+		%MoneyLabel.text = "Scrap: " + str(game_manager.get_money())
+		game_manager.money_change.connect(func(_delta, tot) : %MoneyLabel.text = "Scrap: " + str(tot))
 
 func make_wave_start():
 	game_manager.start_wave()
